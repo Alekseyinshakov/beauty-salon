334996f7d329892f85d633469338f6821b7455d6
Alexey <alexeyinshakov992@gmail.com>
2023-01-02 16:55:38 +0300

    Top fully complete!

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import * as flsFunctions from "./modules/functions.js";
+import {headerAnimate} from "./modules/header-animate.js";
+
+window.onload = function() {
+   flsFunctions.isWebp();
+   headerAnimate()
+};
+
+
+
+
+
---
+++ src/js/modules/header-animate.js
@@ -0,0 +1,31 @@
+export function headerAnimate() {
+   // Animation title and header
+   document.querySelector('.header').classList.add('header-animate');
+   document.querySelector('.header-adaptive').classList.add('header-adaptive-animate');
+   document.querySelector('.top-title').classList.remove('top-title-hide');
+
+
+
+   // Panel
+
+   const panel = document.querySelector(".header__panel")
+
+   document.querySelector('.header__panel-btn').addEventListener("click", () => {
+      panel.classList.remove("hide");
+   })
+
+   document.querySelector(".close-btn").addEventListener("click", () => {
+      panel.classList.add("hide");
+   })
+
+
+   // drop down menu
+
+   document.querySelector('.header-adaptive__panel-btn')
+   .addEventListener('click', () => {
+      console.log('click');
+      document.querySelector('.header-adaptive__menu')
+      .classList.toggle('hide')
+   })
+
+}
